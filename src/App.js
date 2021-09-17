3d2966c6e32ab63521e7552407bf28ad9151bb8f
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2021-09-17 12:27:32 -0300

    trayendo datos de la api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Character } from "./components/Character";
 
 function App() {
+  const baseUrl = 'https://rickandmortyapi.com/api'
+  const [characters, setCharacters] = useState({
+    loading: true,
+    data: null
+  });
+
+  useEffect(() => {
+    fetch(`${baseUrl}/character`)
+    .then( res => res.json())
+    .then(data => setCharacters({
+      loading: false,
+      data: data
+    }))
+
+  }, [])
+
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-r from-gray-800 to-purple-700  h-full">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="p-4 text-white flex justify-center">RICK AND MORTY</h1>
       </header>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-10">
+        {
+          !characters.loading
+          &&
+          characters.data.results.map(character => (
+            <Character key={character.id} info= {character} />
+          ))
+        }
+      </div>
     </div>
   );
 }
